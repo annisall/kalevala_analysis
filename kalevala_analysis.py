9adc6022b652bc658483bbf4c18710dcaa37e6ab
annisall <anniina.sallinen@cs.helsinki.fi>
2019-03-16 19:13:56 +0200

    Removed debug prints and an unused import

--- kalevala_analysis.py
+++ kalevala_analysis.py
@@ -1,7 +1,6 @@
 from os import listdir
 from os.path import isfile, join
 import string
-from functools import reduce
 
 transtbl=str.maketrans('', '', string.punctuation)
 
@@ -22,8 +21,6 @@ def clean_poems(poems):
 	return list(map(lambda line: clean_line(line), poems))
 
 def word_count(lines):
-	
-	#return list(reduce(lambda x, y: x+y, (map(foo, lines))))
 	return list(map(lambda line: len(line.split()), lines))
 
 def count_metrics(poems):
@@ -33,7 +30,6 @@ def count_metrics(poems):
 
 def read_files(directory_path):
 	file_names=[file for file in listdir(directory_path) if isfile(join(directory_path, file)) and '.txt' in file]
-	#poems=[get_poem(join(directory_path, name)) for name in file_names]
 	poems=list(map(lambda name: (name, get_poem(join(directory_path, name))), file_names))
 	return poems
 
@@ -41,5 +37,4 @@ poems_list=read_files('data')
 metrics=count_metrics(poems_list)
 for thing in metrics:
 	print('Name: {0}, line count: {1}, word count: {2}'.format(thing[0], thing[1], thing[2]))
-#print(list(map(lambda poem: (poem[0], sum(word_count(poem[1]))), poems_list)))
 
