80c5937d82cfa1fe6d6b8dfadb72fcf538eaa0f1
annisall <anniina.sallinen@cs.helsinki.fi>
2019-03-16 20:52:44 +0200

    Added word frequence funtionality

--- kalevala_analysis.py
+++ kalevala_analysis.py
@@ -1,14 +1,17 @@
 from os import listdir
 from os.path import isfile, join
 import string
+from functools import reduce
+from collections import Counter
+import pprint
 
 transtbl=str.maketrans('', '', string.punctuation)
 
 def clean_line(line):
 	return line.rstrip('\n').strip()
 
-def remove_punctuations(line):
-	return line[1].translate(transtbl)
+def remove_punctuations(lines):
+	return list(map(lambda line: line.translate(transtbl), lines))
 
 def is_empty(line):
 	return line
@@ -17,24 +20,39 @@ def get_poem(file_path):
 	with open(file_path) as poemfile:
 		return list(filter(is_empty, map(clean_line, poemfile.readlines())))
 
-def clean_poems(poems):
-	return list(map(lambda line: clean_line(line), poems))
-
-def word_count(lines):
-	return list(map(lambda line: len(line.split()), lines))
+def word_count(poem):
+	return list(map(lambda line: len(line.split()), poem))
 
 def count_metrics(poems):
 	return list(map(lambda poem: (poem[0],len(poem[1]), sum(word_count(poem[1]))), poems))
-
-
 
 def read_files(directory_path):
 	file_names=[file for file in listdir(directory_path) if isfile(join(directory_path, file)) and '.txt' in file]
 	poems=list(map(lambda name: (name, get_poem(join(directory_path, name))), file_names))
 	return poems
 
+def flatten_poems(poems):
+	return list(map(lambda poem: (poem[0], ' '.join(poem[1]).split()), poems))
+
+def word_frequency(poems):
+	return list(map(lambda poem: (poem[0], Counter(poem[1])), poems))
+
+def print_poems(poems):
+	for poem in poems:
+		print('Name: {0}, poem: {1}'.format(poem[0], poem[1]))
+
+def pretty_print_frequencies(poem):
+	print('Name of the poem: {0}'.format(poem[0]))
+	pprint.pprint(poem[1])
+
+
+
 poems_list=read_files('data')
 metrics=count_metrics(poems_list)
-for thing in metrics:
-	print('Name: {0}, line count: {1}, word count: {2}'.format(thing[0], thing[1], thing[2]))
+cleaned_poems=list(map(lambda poem: (poem[0], remove_punctuations(poem[1])), poems_list))
+
+flattened=flatten_poems(cleaned_poems)
+
+freqs=(word_frequency(flattened))
+
 
