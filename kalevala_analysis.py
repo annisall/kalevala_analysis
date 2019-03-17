2fc668f073ee787fe041cf158d35736bf2a41f87
annisall <anniina.sallinen@cs.helsinki.fi>
2019-03-17 21:10:02 +0200

    Completed first a and b, added tests

--- kalevala_analysis.py
+++ kalevala_analysis.py
@@ -1,58 +1,108 @@
 from os import listdir
 from os.path import isfile, join
 import string
-from functools import reduce
 from collections import Counter
 import pprint
+from collections import defaultdict
+import numpy as np
 
-transtbl=str.maketrans('', '', string.punctuation)
+transtbl = str.maketrans('', '', string.punctuation)
 
+""" Removes spaces at the beginning and end, removes newlines and makes everything lower case """
 def clean_line(line):
-	return line.rstrip('\n').strip()
+    return line.rstrip('\n').strip().lower()
 
+""" Removes punctuations from list of lines """
 def remove_punctuations(lines):
-	return list(map(lambda line: line.translate(transtbl), lines))
+    return list(map(lambda line: line.translate(transtbl), lines))
 
+""" Checks if given object is empty """
 def is_empty(line):
-	return line
+    return line
 
 def get_poem(file_path):
-	with open(file_path) as poemfile:
-		return list(filter(is_empty, map(clean_line, poemfile.readlines())))
+    with open(file_path) as poemfile:
+        return list(filter(is_empty, map(clean_line, poemfile.readlines())))
 
-def word_count(poem):
-	return list(map(lambda line: len(line.split()), poem))
+""" Joins list elements with space and then splits sentences into single words 
+Input: list of sentences,
+output: list of words in all sentences given as input """
+def combine_and_tokenize(poem):
+    return ' '.join(poem).split()
 
+""" Returns tuple containing name of file (poem), number of (nonempty) rows in a poem 
+and how many words there are in a poem.
+Input: (name, list of lines in a poem)
+output: (name, number of lines, number of words)"""
 def count_metrics(poems):
-	return list(map(lambda poem: (poem[0],len(poem[1]), sum(word_count(poem[1]))), poems))
+    return list(map(lambda poem: (poem[0], len(poem[1]), len(combine_and_tokenize(poem[1]))), poems))
 
+""" Find all files under specified folder and read them 
+Input: path to directory containing data files (poems)
+output: list of tuples (name, list of (nonempty) lines in poems)"""
 def read_files(directory_path):
-	file_names=[file for file in listdir(directory_path) if isfile(join(directory_path, file)) and '.txt' in file]
-	poems=list(map(lambda name: (name, get_poem(join(directory_path, name))), file_names))
-	return poems
+    file_names = (file for file in listdir(directory_path) if isfile(join(directory_path, file)) and '.txt' in file)
+    poems = list(map(lambda name: (name, get_poem(join(directory_path, name))), file_names))
+    return poems
 
-def flatten_poems(poems):
-	return list(map(lambda poem: (poem[0], ' '.join(poem[1]).split()), poems))
+def combine_and_tokenize_all_poems(poems):
+    return list(map(lambda poem: (poem[0], combine_and_tokenize(poem[1])), poems))
 
+""" Calculates word frequencies in all poems 
+input: list of tuples (name, list of (nonempty) lines in poems) 
+output: list of tuples (name, {word:count}) where word is word occurring in poem and count 
+is the number it occurs """
 def word_frequency(poems):
-	return list(map(lambda poem: (poem[0], Counter(poem[1])), poems))
+    return list(map(lambda poem: (poem[0], Counter(poem[1])), poems))
+
+""" Filters words that occur only once """
+def filter_rare_items(word_freqs):
+    return {word: freq for word, freq in word_freqs.items() if freq > 1}
+
+""" Iterates over a poem and appends indexes of recurrences into a list.
+ param poem: list of words,freq words: {word: count}
+ returns {word: [list of indexes]} """
+def find_word_indeces(poem, freq_words):
+    occurrences_list = defaultdict(list)
+    for index, word in enumerate(poem):
+        if(word in freq_words):
+            occurrences_list[word].append(index)
+    return occurrences_list
+
+""" poems: tuple (name, list of words)
+ frequent items: (name: {word:count}) """
+def map_indeces_to_poems(poems, frequent_items):
+    return map(lambda poem: (poem[0], find_word_indeces(poem[1], frequent_items[poem[0]])), poems)
+
+"""calculate sum of distances for all words in a poem"""
+def calculate_avg_distances(words):
+    return {word: (np.sum(np.diff(freq))/(len(freq)-1)) for word, freq in words.items()}
+
+"""dict {nimi: {word: list of indexes}}"""
+def avg_dists_across_poems(poems):
+    return {name: calculate_avg_distances(words) for name, words in poems.items()}
 
 def print_poems(poems):
-	for poem in poems:
-		print('Name: {0}, poem: {1}'.format(poem[0], poem[1]))
+    for poem in poems:
+        print('Name: {0}, poem: {1}'.format(poem[0], poem[1]))
 
-def pretty_print_frequencies(poem):
-	print('Name of the poem: {0}'.format(poem[0]))
-	pprint.pprint(poem[1])
+def map_with_function(function_name, iterable_object):
+    return map(lambda poem: (poem[0], function_name(poem[1])), iterable_object)
 
+if __name__ == '__main__':
+    poems_list = read_files('data')
+    print(poems_list[0])
+    metrics = count_metrics(poems_list)
+    cleaned_poems = list(map_with_function(remove_punctuations, poems_list))
 
+    poem_lists = combine_and_tokenize_all_poems(cleaned_poems)
+    print(poem_lists[0])
 
-poems_list=read_files('data')
-metrics=count_metrics(poems_list)
-cleaned_poems=list(map(lambda poem: (poem[0], remove_punctuations(poem[1])), poems_list))
+    freqs = word_frequency(poem_lists)
 
-flattened=flatten_poems(cleaned_poems)
+    frequent_items = dict(map_with_function(filter_rare_items, freqs))
 
-freqs=(word_frequency(flattened))
+    indeces=dict(map_indeces_to_poems(poem_lists, frequent_items))
 
-
+    #print(indeces['1.txt'])
+    #print(avg_dists_across_poems(indeces)['1.txt'])
---
+++ kalevala_analysis_test.py
@@ -0,0 +1,47 @@
+import unittest
+import kalevala_analysis as kaleva
+from collections import Counter
+
+class kalevalaTest(unittest.TestCase):
+    def test_clean_line_removes_ending_space(self):
+        self.assertEqual(kaleva.clean_line('foobar '), 'foobar')
+
+    def test_clean_line_removes_starting_space(self):
+        self.assertEqual(kaleva.clean_line(' foobar'), 'foobar')
+
+    def test_clean_line_removes_both_spaces(self):
+        self.assertEqual(kaleva.clean_line(' foobar '), 'foobar')
+
+    def test_clean_line_removes_newlines(self):
+        self.assertEqual(kaleva.clean_line('foobar\n\n'), 'foobar')
+
+    def test_clean_line_lowercase(self):
+        self.assertEqual(kaleva.clean_line('FooBar'), 'foobar')
+
+    def test_remove_punctuations_basic(self):
+        self.assertEqual(kaleva.remove_punctuations(['my name is Bond, James Bond!']), ['my name is Bond James Bond'])
+
+    def test_remove_punctuations_escaped(self):
+        self.assertEqual(kaleva.remove_punctuations(['my name is O\'Malley, Thomas O\'Malley!']), ['my name is OMalley Thomas OMalley'])
+
+    def test_combine_and_tokenize_two_sentences(self):
+        self.assertEqual(kaleva.combine_and_tokenize(['This is number one', 'This is number two', 'This is number three']), 
+            ['This', 'is', 'number', 'one', 'This', 'is', 'number', 'two','This', 'is', 'number', 'three'])
+
+    def test_count_metrics(self):
+        self.assertEqual(kaleva.count_metrics([('first poem', ['This is the end', 'hold your breath', 'and count to ten']), 
+            ('second poem', ['This is the way the world ends', 'This is the way the world ends', 'This is the way the world ends', 'Not with a bang but a whimper'])]),
+             [('first poem', 3, 11), ('second poem', 4, 28)])
+
+    def test_word_frequency_two_sentences(self):
+        self.assertEqual(kaleva.word_frequency([('first poem', ['this', 'is', 'the', 'way', 'the', 'world', 'ends']), 
+            ('second poem', ['not', 'with', 'a', 'bang', 'but', 'a', 'whimper'])]),
+             [('first poem', Counter({'the': 2,'this':1, 'is': 1, 'way': 1, 'world':1, 'ends': 1})),
+              ('second poem', Counter({'a': 2, 'not': 1, 'with': 1, 'bang': 1, 'but': 1, 'whimper': 1}))])
+
+    def test_filter_rare_items(self):
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
